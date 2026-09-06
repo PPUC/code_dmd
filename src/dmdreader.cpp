849100a6745fc5319678da22ca7bad2835ffa902
Jan Vos <142336369+PastorL69@users.noreply.github.com>
2026-09-06 12:17:28 +0200

    Improve WPC RDATA autodetect (#57)
    * double rdata
    
    * add comment

--- src/dmdreader.cpp
+++ src/dmdreader.cpp
@@ -349,9 +349,9 @@ DmdType detect_dmd() {
              (rclk < 2050) && (rdata > 55) && (rdata < 65)) {
     return DMD_SAM;
 
-    // WPC: DOTCLK: 500000 | RCLK: 3900 | RDATA: 120
+    // WPC: DOTCLK: 500000 | RCLK: 3900 | RDATA: 120 (doubled in some cases)
   } else if ((dotclk > 450000) && (dotclk < 550000) && (rclk > 3800) &&
-             (rclk < 4000) && (rdata > 115) && (rdata < 130)) {
+             (rclk < 4000) && (rdata > 115) && (rdata < 260)) {
     return DMD_WPC;
 
     // DOTMATION: DOTCLK: 1900000 | RCLK: 9950 | RDATA: 155
